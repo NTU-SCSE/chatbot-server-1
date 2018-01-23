3e369d72c46c8c64f4ef833005d874278f8d760e
dbakti7 <dbakti1605@gmail.com>
2018-01-23 14:49:23 +0800

    Added default fallback: redirect to Google search results.

--- main.go
+++ main.go
@@ -157,7 +157,9 @@ func webhookHandler(rw http.ResponseWriter, req *http.Request) {
     params := make([]string, 0)
     paramsJSON.ForEach(func(key, value gjson.Result) bool {
         for _, elem := range value.Array() {
-            params = append(params, elem.String())
+            if(elem.String() != "") {
+                params = append(params, strings.ToLower(elem.String()))
+            }
         }
         return true
     })
@@ -174,14 +176,16 @@ func webhookHandler(rw http.ResponseWriter, req *http.Request) {
         contexts = append(contexts, gjson.Get(elem.String(), "name").String())
     }
 
-    fmt.Println(intent)
-    fmt.Println(originalRequest)
-    fmt.Println(params)
-    fmt.Println(contexts)
-
     // preparing response
     var resultMap map[string]interface{}
     resultMap = make(map[string]interface{})
+
+    // for debugging
+    fmt.Println(originalRequest)
+    fmt.Println(params)
+    fmt.Println(intent)
+
+    // TODO: fill with proper values here
     resultMap["displayText"] = "Test Response"
     resultMap["speech"] = "Response not found"
     resultMap["data"] = ""
@@ -189,23 +193,42 @@ func webhookHandler(rw http.ResponseWriter, req *http.Request) {
     resultMap["source"] = "Hello"
 
     if(strings.Compare(intent.String(), "Course") == 0) {
-        // super hack here. TODO: get course code properly from params
+        // course related
         for _, elem := range params {
             courseCode := parseCourseCode(elem)
             if(courseCode != "") {
-                fmt.Println(courseCode)
                 course, _ := db.GetCourseByCode(courseCode)
-                fmt.Println(course)
                 resultMap["speech"] = course.Description
             }
         }
     } else {
+        // other queries
         for _, elem := range all {
             dbValue := strings.Split(elem.Query, ",")    
             sort.Strings(dbValue)
+            for index, _ := range dbValue {
+                dbValue[index] = strings.ToLower(dbValue[index])
+            }
             if(strings.Compare(intent.String(), elem.Intent) == 0 && reflect.DeepEqual(params, dbValue)) { //&& strings.Compare(qwordValue, elem.QWord) == 0) {
                 resultMap["speech"] = elem.Value
             }
+        }
+    }
+
+    // default fallback: direct to google search, get the first result
+    if strings.Compare(resultMap["speech"].(string), "Response not found") == 0 {
+        resp, err := http.Get("https://www.googleapis.com/customsearch/v1?q=" + 
+            strings.Replace(originalRequest.String(), " ", "+", -1) + "&cx=000348109821987500770%3Ar1ufthpxqxg&key=AIzaSyDW0l64m7xweAo28Z_q3yAskU_d5fbevGw")
+        if err != nil {
+            // handle error
+        }
+        defer resp.Body.Close()
+        body, err := ioutil.ReadAll(resp.Body)
+        results := gjson.Get(string(body[:]), "items")
+        for _, elem := range results.Array() {
+            link := gjson.Get(elem.String(), "link").String()
+            resultMap["speech"] = "You can find out more about it at " + link + "\r\n"
+            break
         }
     }
 
